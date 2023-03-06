0a7282d4d2eccb591228f7d6df080e54724428d6
taylortempleton <114506693+taylortempleton@users.noreply.github.com>
2023-03-06 09:40:44 -0500

    Add files via upload

---
+++ Lab01/design.sv
@@ -0,0 +1,22 @@
+// Code your design here
+module register (
+// Verilog2001: port and variable declarations in module definition
+// SystemVerilog: logic data type
+                output logic [7:0] out,
+                input  logic [7:0] data ,
+                input        clk  ,
+                input        enable  ,
+                input        rst_
+                );
+// SystemVerilog: time unit and time precision declaration
+timeunit 1ns;
+timeprecision 100ps;
+
+// SystemVerilog: always_ff - sequential behavior intent specification
+always_ff @(posedge clk, negedge rst_)
+    if (!rst_)
+      out <= 0;
+    else if (enable)
+      out <= data;
+
+endmodule
